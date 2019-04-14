9287cdc6350ba9af8743f1a4908c4bda77588953
Gabriel Le Roux <leroux@zib.de>
2019-04-14 19:17:29 +0200

    Blender file and pixel generator working

---
+++ blender/py/bpy_functions.py
@@ -0,0 +1,31 @@
+import bpy
+import bmesh
+
+def create_sphere(name, location, diameter=1, u_segments=32, v_segments=16):
+	# Create an empty mesh and the object.
+	scn = bpy.context.scene
+	mesh = bpy.data.meshes.new(name)
+	basic_sphere = bpy.data.objects.new(name, mesh)
+
+	# Add the object into the scene.
+	bpy.context.scene.objects.link(basic_sphere)
+	bpy.context.scene.objects.active = basic_sphere
+	basic_sphere.select = True
+
+	# Construct the bmesh sphere and assign it to the blender mesh.
+	bm = bmesh.new()
+	bmesh.ops.create_uvsphere(bm, u_segments=u_segments, v_segments=v_segments, diameter=diameter)
+	bm.to_mesh(mesh)
+	bm.free()
+
+	basic_sphere.location = location
+	basic_sphere.data.materials.append(bpy.data.materials.get("pixel").copy())
+
+
+# obj = bpy.context.active_object
+# mat = obj.active_material
+# mesh = obj.data
+
+# dup = bpy.data.objects.new(obj.name, mesh.copy())
+# dup.active_material = bpy.data.materials.get("pixel").copy()
+# scn.objects.link(dup)
---
+++ blender/py/pixel_generator.py
@@ -0,0 +1,33 @@
+import bpy
+import bpy_functions as bf
+import numpy as np
+import os.path, sys
+import json
+
+lib_path = bpy.path.abspath("//../py/")
+assert os.path.exists(lib_path), 'Make sure "." path is where notebooks are!'
+if not lib_path in sys.path:
+    sys.path.insert(0, lib_path)
+
+import audio, features, settings, streaming, util
+
+sphere = bpy.data.objects['Sphere']
+nr_verts = len(sphere.data.vertices)
+nr_pixels = 5*60
+
+radius = np.linalg.norm(sphere.data.vertices[0].co)
+
+pixels_polar = []
+for i in range(nr_pixels):
+	vert_idx = np.random.randint(nr_verts)
+	co = sphere.data.vertices[vert_idx].co
+
+	bf.create_sphere("pixel_"+str(i+1).zfill(3), co, diameter=0.2, u_segments=16, v_segments=8)
+
+	theta = np.arccos(co[2]/radius)
+	phi = np.arctan(co[1]/co[0])
+
+	pixels_polar.append({"idx" : str(i+1).zfill(3), "theta" : theta, "phi" : phi})
+
+with open(bpy.path.abspath("//../data/") + 'blender_polar.json', 'w') as fp:
+    json.dump(pixels_polar, fp, sort_keys=True, indent=4 * ' ')
