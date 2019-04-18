326341ec2c4bf869e5c6f1c724536b70354572d2
Andreas Steiner <andstein@google.com>
2019-04-18 20:21:52 +0200

    simplifies config.py (breaks lighter.py)

--- py/config.py
+++ py/config.py
@@ -1,52 +1,17 @@
+"""Common config, backed by ./config.json."""
 
-from __future__ import print_function
-import json, fcntl, os
+import fcntl, json, os, sys
 
-
-CHANNELS = [
-        # REAL channels
-        'intensity',
-        'strobe',
-        'red',
-        'green',
-        'blue',
-        'white',
-        'pan',
-        'tilt',
-        'speed',
-        # PSEUDO channels
-        'move amp',
-        'move freq',
-        'pulse amp',
-        'pulse freq',
-        ]
-CHANNELS_REAL = 9
-
-#  pan : 0=148: 0
-#        37=184: pi/2
-#        72=221: pi
-#        109=255: 3pi/2
-#  tilt: 38: 0
-#        86: pi/4
-#        133: pi/2
-#        191: 3pi/4
-#        229: pi
-INITIAL_VALUES = dict(
-        intensity=255,
-        white=255,
-        pan=72,
-        tilt=133
-        )
+import util
 
 
-class Config(object):
+class Config:
 
-    def __init__(self):
-        self.path = 'config.json'
+    def __init__(self, logger):
+        self.logger = logger
+        self.path = os.path.join(os.path.dirname(__file__), 'config.json')
         self.mtime = 0
         self.read()
-        if 'presets' not in self.data:
-            self.data['presets'] = {}
 
     def read(self):
         mtime = os.stat(self.path).st_mtime
@@ -68,35 +33,30 @@ class Config(object):
             finally:
                 fcntl.lockf(f, fcntl.LOCK_UN)
 
-    def getpreset(self, key):
-        key = str(key)  # javascript dictionaries ...
-        self.read()
-        preset = dict(zip(CHANNELS, [0]*len(CHANNELS)))
-        preset.update(**INITIAL_VALUES)
-        preset.update(**self.data['presets'].get(key, {}))
-        return preset
-
-    def setpreset(self, key, value):
-        key = str(key)
-        self.data['presets'][key] = value
-        self.write()
-
     def get(self, key, default):
         key = str(key)
         self.read()
         return self.data.get(key, default)
 
-    def set(self, key, value):
+    def __getitem__(self, key):
+        key = str(key)
+        self.read()
+        return self.data[key]
+
+    def __setitem__(self, key, value):
         key = str(key)
         self.data[key] = value
         self.write()
 
-
-conf = Config()
+    def __delitem__(self, key):
+        del self.data[key]
+        self.write()
 
 
 if __name__ == '__main__':
-    import readline, sys
+
+    logger = util.createLogger('config')
+    conf = Config(logger)
 
     while True:
         sys.stdout.write('>>> ')
@@ -106,9 +66,11 @@ if __name__ == '__main__':
         if words == ['quit']:
             break
         elif words == ['list']:
-            print('-> ', conf.data.keys())
+            print('-> keys:', ' '.join(conf.data.keys()))
         elif words[0] == 'get' and len(words) == 2:
-            print('-> ', conf.get(words[1], '?'))
+            print('-> ', conf[words[1]])
+        elif words[0] == 'del' and len(words) == 2:
+            del conf[words[1]]
         elif words[0] == 'set' and len(words) == 3:
             value = words[2]
             try:
@@ -120,20 +82,14 @@ if __name__ == '__main__':
                     print('-> float(%f)' % value)
                 except ValueError:
                     print('-> str(%s)' % value)
-            conf.set(words[1], value)
-        elif words == ['listpresets']:
-            print('-> ', ', '.join(sorted(conf.data['presets'].keys())))
-        elif words[0] == 'getpreset' and len(words) == 2:
-            print('-> ', ', '.join(conf.getpreset(words[1])))
+            conf[words[1]] = value
         elif words == ['help']:
             print('')
             print('list')
             print('get KEY')
+            print('del KEY')
             print('set KEY VALUE')
-            print('list')
-            print('getpreset PRESET')
             print('quit')
             print('')
         else:
             print('*** unknown command - try "help"')
-
