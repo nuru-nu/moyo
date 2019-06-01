81bdedfd2a2fa6350531c24f2f129dfb69d54f38
Andreas Steiner <andstein@google.com>
2019-06-01 12:16:07 +0200

    adds TMUX-based launch.sh

---
+++ launch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+
+SESSION='rizhom'
+
+tmux start-server
+
+tmux new-session -d -s "$SESSION" -n prod
+
+# create columns
+tmux selectp -t 1
+tmux splitw -h
+
+# column 1 : recorder, monitor & player
+tmux selectp -t 1
+tmux send-keys ". env/bin/activate && cd py && echo python recorder2.py" C-m
+tmux splitw -v -p 66
+tmux send-keys ". env/bin/activate && cd py && echo python monitor.py" C-m
+tmux splitw -v
+tmux send-keys ". env/bin/activate && cd py && echo python player.py" C-m
+
+# column 2 : fc server & run_animations
+tmux selectp -t 4
+tmux send-keys "cd fc && echo WOULD RUN FC SERVER HERE..." C-m
+tmux splitw -v
+tmux send-keys ". env/bin/activate && cd py && echo python run_fadecandy_animations.py" C-m
+
+# # window 2 : git, jupyter
+# tmux new-window -t "$SESSION":1 -n dev
+# tmux send-keys "echo WOULD RUN JUPYTER HERE..." C-m
+# tmux splitw -v -p 80
+# tmux send-keys "git status" C-m
+
+tmux attach-session -t "$SESSION"
+
