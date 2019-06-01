735eb49d9fa7129e871560f24c3528df338a4066
Andreas Steiner <andstein@google.com>
2019-06-01 12:16:07 +0200

    adds TMUX-based launch.sh

--- launch.sh
+++ launch.sh
@@ -15,17 +15,17 @@ tmux splitw -h
 
 # column 1 : recorder, monitor & player
 tmux selectp -t 1
-tmux send-keys ". env/bin/activate && cd py && echo python recorder2.py" C-m
+tmux send-keys ". env/bin/activate && cd py && python recorder2.py" C-m
 tmux splitw -v -p 66
-tmux send-keys ". env/bin/activate && cd py && echo python monitor.py" C-m
+tmux send-keys ". env/bin/activate && cd py && python monitor.py" C-m
 tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py && echo python player.py" C-m
+tmux send-keys ". env/bin/activate && cd py && python player.py" C-m
 
 # column 2 : fc server & run_animations
 tmux selectp -t 4
-tmux send-keys "cd fc && echo WOULD RUN FC SERVER HERE..." C-m
+tmux send-keys "cd fadecandy && ./fadecandy_server config.json" C-m
 tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py && echo python run_fadecandy_animations.py" C-m
+tmux send-keys ". env/bin/activate && cd py && python run_fadecandy_animations.py" C-m
 
 # # window 2 : git, jupyter
 # tmux new-window -t "$SESSION":1 -n dev
