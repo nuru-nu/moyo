5bd2995e2b7618d5c6351d9738808f197dd386f2
Andreas Steiner <andstein@google.com>
2019-06-01 16:54:41 +0200

    makes up arrow work for command repetition

--- launch.sh
+++ launch.sh
@@ -15,17 +15,17 @@ tmux splitw -h
 
 # column 1 : recorder, monitor & player
 tmux selectp -t 1
-tmux send-keys ". env/bin/activate && cd py && python recorder2.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-m "python recorder2.py" C-m
 tmux splitw -v -p 66
-tmux send-keys ". env/bin/activate && cd py && python monitor.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-M "python monitor.py" C-m
 tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py && python player.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-M "python player.py" C-m
 
 # column 2 : fc server & run_animations
 tmux selectp -t 4
-tmux send-keys "cd fadecandy && ./fadecandy_server config.json" C-m
+tmux send-keys "cd fadecandy" C-M "./fadecandy_server config.json" C-m
 tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py && python run_fadecandy_animations.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-M "python run_fadecandy_animations.py" C-m
 
 # # window 2 : git, jupyter
 # tmux new-window -t "$SESSION":1 -n dev
