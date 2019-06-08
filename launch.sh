267f103f9f491347f5a9f2900df95fb58ec8995e
Gabriel Le Roux <lrxgab005@gmail.com>
2019-06-08 11:38:43 +0200

    improves arrangement launch.sh

--- launch.sh
+++ launch.sh
@@ -9,23 +9,30 @@ tmux start-server
 
 tmux new-session -d -s "$SESSION" -n prod
 
+CM='C-m'
+# CM=
+
 # create columns
 tmux selectp -t 1
 tmux splitw -h
 
-# column 1 : recorder, monitor & player
+# column 1 : recorder, monitor & players
 tmux selectp -t 1
-tmux send-keys ". env/bin/activate && cd py" C-m "python recorder2.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-m "python recorder2.py" $CM
 tmux splitw -v -p 66
-tmux send-keys ". env/bin/activate && cd py" C-M "python monitor.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-M "python monitor.py"
 tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py" C-M "python player.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-M "python player.py out1" $CM
+tmux splitw -h
+tmux send-keys ". env/bin/activate && cd py" C-M "python player.py out2    " $CM
 
-# column 2 : fc server & run_animations
-tmux selectp -t 4
-tmux send-keys "cd fadecandy" C-M "./fadecandy_server config.json" C-m
+# column 2 : fc server, run_animations & dmx
+tmux selectp -t 5
+tmux send-keys "cd fadecandy" C-M "./fcserver config.json" $CM
+tmux splitw -v -p 66
+tmux send-keys ". env/bin/activate && cd py" C-M "python run_fadecandy_animations.py" $CM
 tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py" C-M "python run_fadecandy_animations.py" C-m
+tmux send-keys ". env/bin/activate && cd py" C-M "python dmx.py" $CM
 
 # # window 2 : git, jupyter
 # tmux new-window -t "$SESSION":1 -n dev
