f588fac172b9a5344a5e979784b186daa7518741
Andreas Steiner <andstein@google.com>
2020-01-11 17:32:07 +0100

    makes arduino/dmx optional & bugfix static http server.

--- launch.sh
+++ launch.sh
@@ -15,10 +15,14 @@ CM='C-m'
 MACHINE="${MACHINE:-$(uname -n)}"
 
 FADECANDY='--fadecandy'
+ARDUINO=yes
+DMX=yes
 
 case "$MACHINE" in
 cervelat*)
   FADECANDY=
+  ARDUINO=
+  DMX=
   ;;
 esac
 
@@ -30,7 +34,7 @@ tmux splitw -h
 tmux selectp -t 1
 tmux send-keys ". env/bin/activate && cd py" C-m 'python restarter.py $(which python) recorder2.py' $CM
 tmux splitw -v -p 66
-tmux send-keys ". env/bin/activate && cd py" C-m "cd js" C-M "python -m http.server" $CM
+tmux send-keys ". env/bin/activate && cd js" C-m "cd js" C-M "python -m http.server" $CM
 tmux splitw -v
 tmux send-keys ". env/bin/activate && cd py" C-M 'python restarter.py $(which python) player.py out1' $CM
 tmux splitw -h
@@ -40,13 +44,17 @@ tmux send-keys ". env/bin/activate && cd py" C-M 'python restarter.py $(which py
 tmux selectp -t 5
 if [ -n -z "$FADECANDY" ]; then
   tmux send-keys ". env/bin/activate && cd fadecandy" C-M "python ../py/restarter.py ./fcserver config.json" $CM
+  tmux splitw -v -p 66
 fi
-tmux splitw -v -p 66
 tmux send-keys ". env/bin/activate && cd py" C-M "python restarter.py $(which python) animator.py $FADECANDY" $CM
-tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py" C-M 'python restarter.py $(which python) dmx.py' $CM
-tmux splitw -v
-tmux send-keys ". env/bin/activate && cd py" C-M 'python restarter.py $(which python) arduino_signals.py' $CM
+if [ -n -z "$DMX" ]; then
+  tmux splitw -v
+  tmux send-keys ". env/bin/activate && cd py" C-M 'python restarter.py $(which python) dmx.py' $CM
+fi
+if [ -n -z "$ARDUINO" ]; then
+  tmux splitw -v
+  tmux send-keys ". env/bin/activate && cd py" C-M 'python restarter.py $(which python) arduino_signals.py' $CM
+fi
 
 # # window 2 : git, jupyter
 # tmux new-window -t "$SESSION":1 -n dev
