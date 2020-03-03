500ce5844347bca26ad643c3c24bc6c303163685
Gabriel Le Roux <lrxgab005@gmail.com>
2020-03-03 19:55:14 +0100

    Code cleaned

--- cc/kinect.cc
+++ cc/kinect.cc
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <signal.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <math.h>
 
 #include <opencv2/opencv.hpp>
 
@@ -15,6 +19,28 @@ bool running = true;
 void sigint_handler(int s) {
   std::cerr << "Caught CTRL-C : Shutting down..." << std::endl;
   running = false;
+}
+
+std::string datetime_str(){
+  char buffer[26];
+  int millisec;
+  struct tm* tm_info;
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+
+  millisec = lrint(tv.tv_usec/1000.0); // Round to nearest millisec
+  if (millisec>=1000) { // Allow for rounding up to nearest second
+    millisec -=1000;
+    tv.tv_sec++;
+  }
+
+  tm_info = localtime(&tv.tv_sec);
+
+  strftime(buffer, 26, "%Y:%m:%d_%H:%M:%S", tm_info);
+  std::string dt_string = buffer;
+  std::cout << dt_string << std::endl;
+  return dt_string + ":" + std::to_string(millisec);
 }
 
 int main() {
@@ -49,7 +75,8 @@ int main() {
     }
 
     if (viewer.should_store()) {
-      hardware.pcl();
+      pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointcloud = hardware.pcl();
+      hardware.write_pcl("../../data/pcls/pcl_" + datetime_str() + ".ply", pointcloud);
       viewer.clear_store();
     }
   }
