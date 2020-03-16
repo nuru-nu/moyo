42742c6b2e7809cddc3bd8bf9d38cf8fa6d2c196
Andreas Steiner <andstein@google.com>
2020-03-16 15:11:10 +0100

    Adds 'sonar' widget.

--- static/app.js
+++ static/app.js
@@ -3,23 +3,30 @@ import { Console, h, Stats } from './smanmi/util.js'
 import { Network } from './smanmi/network.js'
 import { Monitor } from './smanmi/monitor.js'
 
+import { Sonar, Debug } from './widgets.js'
 import { Leds } from './leds.js'
 
 window.console = Console('#console')
 
-let monitor = Monitor('#monitor')
+let monitor = Monitor('#monitor', {
+  presets: {
+    default: new Set(['loud', 'low', 'high']),
+    states: new Set(['drone1', 'drone2', 'into']),
+  },
+})
 let leds = Leds('#leds')
 
-let network = Network('#connection_state', { record_timestamps: true })
+const record_timestamps = true
+
+let sonar = Sonar('#controls')
+
+let network = Network('#connection_state', { record_timestamps })
 .listen('animation', Stats('#animation_stats'))
 .listen('animation', leds.listener)
 .listen('signals', Stats('#signals_stats'))
 .listen('signals', monitor.listener)
+.listen('signals', sonar.listener)
 
+Debug('#controls', { network, record_timestamps })
 monitor.sendto(network.sender)
-
-const controls = h.div().of(
-  h.button('download').of('download traces')
-).into('#controls').els
-controls.download.addEventListener('click', network.download_timestamps)
-
+sonar.sendto(network.sender)
