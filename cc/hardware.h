a73cafd4d5f571193f28527710e0b237fecb2c0b
Andreas Steiner <andstein@google.com>
2020-04-17 11:36:05 +0200

    makes PCL / NiTE optional

--- cc/hardware.h
+++ cc/hardware.h
@@ -1,6 +1,8 @@
 #ifndef SMANMI_HARDWARE_H
 #define SMANMI_HARDWARE_H
 
+#include "conf.h"
+
 #include <memory>
 
 #include <libfreenect2/libfreenect2.hpp>
@@ -9,8 +11,10 @@
 #include <libfreenect2/packet_pipeline.h>
 #include <libfreenect2/logger.h>
 
+#ifdef USE_PCL
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#endif
 
 #include <opencv2/opencv.hpp>
 
@@ -28,14 +32,18 @@ class Hardware {
     cv::Mat ir();
     // Returns rgb data. Invalidated when `next()` is called.
     cv::Mat rgb();
+
+    // Shuts down the device, irreversibly.
+    void close();
+
+#ifdef USE_PCL
     // Returns a RGB point cloud. Invalidated when `next()` is called.
     pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pcl();
     // Writes a pcl to file
     void write_pcl(std::string path, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointcloud);
     // Starts a recording of each frame when  next() is called
     void record_pcl(const std::string path, const int nr_frames);
-    // Shuts down the device, irreversibly.
-    void close();
+#endif
 
   private:
     const bool rgb_;
@@ -45,12 +53,14 @@ class Hardware {
     std::unique_ptr<libfreenect2::SyncMultiFrameListener> listener_;
     std::unique_ptr<libfreenect2::Freenect2Device> dev_;
     libfreenect2::Registration* registration;
-    
+
+#ifdef USE_PCL
     bool recording = false;
     int nr_rec_frames_;
     std::string rec_path;
     std::vector<std::string> rec_names_;
     std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> pointclouds_;
+#endif
 };
 
 #endif
