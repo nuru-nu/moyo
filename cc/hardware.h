dffb832d7e872b37d8d9d8b39ec49cb752f16de9
Andreas Steiner <andstein@google.com>
2020-04-17 20:01:19 +0200

    Revives previous commits (JSON, override, --simulate, --no-gui, USE_PCL, USE_NITE).

--- cc/hardware.h
+++ cc/hardware.h
@@ -3,19 +3,23 @@
 
 #include <memory>
 
+#ifdef USE_PCL
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#endif
 
 #include <opencv2/opencv.hpp>
 
+#ifdef USE_NITE
 #include "NiTE.h"
+#endif
 
 class Hardware {
   public:
     // Also initializes Kinect and exits program in case of error.
-    Hardware();
+    Hardware(bool simulate = false);
 
-    // Waits for another frame. Returns `false` in case of error.
+    // Waits for another frame. Returns 0 if successful, != 0 if timeout.
     int next();
 
     // // Returns IR data. Invalidated when `next()` is called.
@@ -24,28 +28,38 @@ class Hardware {
     // cv::Mat rgb();
     // Returns depth data. Invalidated when `next()` is called.
     cv::Mat depth();
+
+    // Shuts down the device, irreversibly.
+    void close();
+
+#ifdef USE_PCL
     // Returns a point cloud.
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcl();
     // Writes a pcl to file
     void write_pcl(std::string path, pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud);
     // Starts a recording of each frame when  next() is called
     void record_pcl(const std::string path, const int nr_frames);
-    // Shuts down the device, irreversibly.
-    void close();
+#endif
 
-  private:   
+  private:
+    const bool simulate_;
+    cv::Mat simulated_depth_, simulated_ir_, simulated_rgb_;
     bool recording = false;
     int nr_rec_frames_;
-    openni::VideoFrameRef depthFrame_;
     std::string rec_path_;
     std::vector<std::string> rec_names_;
-    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> pointclouds_;
 
+#ifdef USE_NITE
+    openni::VideoFrameRef depthFrame_;
     nite::UserTrackerFrameRef userTrackerFrame_;
     nite::Status niteRc_;
     nite::UserTracker userTracker_;
+#endif
 
+#ifdef USE_PCL
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> pointclouds_;
     void recorder();
+#endif
 
 };
 
--- cc/kinect.cc
+++ cc/kinect.cc
@@ -13,7 +13,8 @@
 #include "util.h"
 #include "viewer.h"
 
-const int kSignalinPort = 6101;
+const int kSignalinPort = 6100;
+const int kCmdPort = 6111;
 bool running = true;
 
 
@@ -22,15 +23,42 @@ void sigint_handler(int s) {
   running = false;
 }
 
-int main() {
-  const int signalin_port = 6101;
+int main(const int argc, const char** const argv) {
 
-  Hardware hardware;
+#ifdef USE_PCL
+  std::cout << "Compiled with USE_PCL" << std::endl;
+#endif
+
+#ifdef USE_NITE
+  std::cout << "Compiled with USE_NITE" << std::endl;
+#endif
+
+  bool simulate = false, gui = true;
+  const std::vector<std::string> args(argv + 1, argv + argc);
+  for (const auto& arg : args) {
+    if (arg == "--no-gui") {
+      gui = false;
+    } else if (arg == "--simulate") {
+      simulate = true;
+    } else if (arg == "--help") {
+      std::cout << R"(kinect sensor; available options:
+  --no-gui: don't show GUI; useful if X not available
+  --simulate: don't actually try to connect to Kinect
+)" << std::endl;
+      return 0;
+    } else {
+      std::cerr << "Unknown option : " << arg << std::endl;
+      return -1;
+    }
+  }
+
+
+  Hardware hardware(simulate);
 
 
   Features features;
-  Viewer viewer;
-  SignalSender sender(kSignalinPort);
+  Viewer viewer(gui);
+  SignalSender sender(kSignalinPort, kCmdPort);
 
   signal(SIGINT, sigint_handler);
   while (running && !viewer.should_quit()) {
@@ -53,6 +81,7 @@ int main() {
       features.reset();
     }
 
+#ifdef USE_PCL
     if (viewer.should_store()) {
       pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud = hardware.pcl();
       hardware.write_pcl("../../data/pcls", pointcloud);
@@ -61,8 +90,8 @@ int main() {
     if (viewer.should_record()) {
       hardware.record_pcl("../../data/pcls", 60);
     }
+#endif
   }
-
 
   hardware.close();
   return 0;
