ae0f98b3b9cc9e8c70d544a5a0fc7d0d333a8810
Andreas Steiner <andstein@google.com>
2020-04-17 16:24:46 +0200

    Adds kinect --no-gui.

--- cc/viewer.h
+++ cc/viewer.h
@@ -7,25 +7,31 @@
 
 #include "features.h"
 
+// Shows Kinect data and lets change parameters via keyboard.
+// In the default GUI mode, the data is shown in a OpenCV window. If GUI mode
+// is disabled, then data is stored upon request in a file `kinect_frame.jpg`.
+// In both modes input is taken directly from the keyboard.
 class Viewer {
   public:
-    Viewer();
+    Viewer(bool gui = true);
     void update(const cv::Mat& img, const Features& features);
     bool should_quit() const { return should_quit_; }
     bool should_store() const { return should_store_; }
     bool should_record() const { return should_record_; }
     bool should_reset() const { return should_reset_; }
-    
+
   private:
     void draw_process_key();
     void update_graphs(const cv::Mat& img, const Features& features);
 
+    const bool gui_;
     float hz_;
     std::chrono::high_resolution_clock::time_point t0_, last_t_;
     bool should_quit_ = false;
     bool should_store_ = false;
     bool should_record_ = false;
     bool should_reset_ = false;
+    bool should_dump_ = false;
     float last_presence_x_ = 0;
     cv::Mat graphs_;
 };
