4a7e8da5107ea00a5d1a0f963f3b5d762fa9b6b3
Andreas Steiner <andstein@google.com>
2020-04-19 18:22:49 +0200

    Improves wav2features performance 10x.

--- py/nuru/features.py
+++ py/nuru/features.py
@@ -44,12 +44,46 @@ def mfccs(data, logmel=None, num_ceps=12, **kwargs):
     return mfcc[:, 1: (num_ceps + 1)]
 
 
+assert 2 ** int(np.log(settings.buf_size) / np.log(2)) == settings.buf_size, (
+    f'settings.buf_size={settings.buf_size} is not a power of 2')
+
+hann_window = mel_features.periodic_hann(settings.buf_size)
+mel_matrix = mel_features.spectrogram_to_mel_matrix(
+    num_mel_bins=settings.num_mel_bins,
+    num_spectrogram_bins=settings.buf_size // 2 + 1,
+    audio_sample_rate=settings.rate,
+    lower_edge_hertz=settings.lower_edge_hertz,
+    upper_edge_hertz=settings.upper_edge_hertz
+)
+
+
 @perf.measure('wav2features')
 def wav2features(wav, hop_size):
     """Calculates audio features.
 
     Args:
       wav: ndarray of type float
+    """
+    assert wav.dtype == np.float32, wav.dtype
+    assert len(wav) == settings.buf_size, len(wav)
+    spectrogram = np.abs(np.fft.rfft(hann_window * wav))
+    logmel = np.log(spectrogram.reshape((1, -1)).dot(mel_matrix))
+    return Features(
+        wav=wav[:hop_size],
+        logmel=logmel[0],
+        # logmel2=log_mel_spectrogram2(wav)[0],
+        logmel2=0,
+        mfccs=mfccs(wav, logmel)[0],
+    )
+
+
+@perf.measure('wav2features_old')
+def wav2features_old(wav, hop_size):
+    """Less efficient original implementation of wav2features.
+
+    This original implementation used the `log_mel_spectrogram()` provided
+    in `audioset.mel_features` that is actually a FFTST suitable to computing
+    the FFT over multiple frames.
     """
     assert wav.dtype == np.float32, wav.dtype
     logmel = log_mel_spectrogram(wav)
