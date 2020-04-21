d449f6ad69edfe9aad49e3abe466364c883da04d
nuru <nuru@smokeandmirrors.ch>
2020-04-21 19:20:13 +0200

    Merge conflic solved. Now cmake Nite error again...

--- cc/hardware.cc
+++ cc/hardware.cc
@@ -3,65 +3,36 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
-#include <unistd.h>
 
-#ifdef USE_PCL
 #include <pcl/io/pcd_io.h>
 #include <pcl/io/ply_io.h>
 #include <pcl/console/print.h>
 #include <pcl/console/parse.h>
 #include <pcl/console/time.h>
-#endif
 
 #include "util.h"
 
+namespace {
 
-Hardware::Hardware(const bool simulate) : simulate_(simulate) {
-  if (simulate_) {
-    std::cerr << "SIMULATING kinect data" << std::endl;
-    simulated_depth_ = cv::Mat(480, 640, CV_32FC1);
-    simulated_ir_ = cv::Mat(480, 640, CV_32FC1);
-    simulated_rgb_ = cv::Mat(480, 640, CV_8UC4);
-    return;
-  }
+const bool kEnableDepth = true;
 
-#ifndef USE_NITE
-  std::cerr << "### Must be called with --simulate or compiled with USE_NITE"
-    << std::endl;
-  exit(-1);
-#else
+}  // namespace
+
+
+Hardware::Hardware(void) {
 
   nite::NiTE::initialize();
 
-  niteRc_ = userTracker_.create();
-  if (niteRc_ != nite::STATUS_OK)
+  nite::Status niteRc = userTracker_.create();
+  if (niteRc != nite::STATUS_OK)
   {
-      printf("niteRc %d \n", niteRc_);
+      printf("niteRc %d \n", niteRc);
       printf("Couldn't create user tracker\n");
   }
-#endif
 }
 
-int Hardware::next() {
-#ifdef USE_PCL
-  if(recording)
-    recorder();
-#endif
-
-  if (simulate_) {
-    usleep(1e6 / 60);
-    return 0;
-  }
-
-#ifdef USE_NITE
-  return userTracker_.readFrame(&userTrackerFrame_);
-#endif
-  std::cerr << "### Hardware::next()" << std::endl;
-  exit(-1);
-}
-
-#ifdef USE_PCL
 void Hardware::recorder(){
+
   pointclouds_.push_back(pcl());
   rec_names_.push_back(datetime_str());
 
@@ -76,11 +47,75 @@ void Hardware::recorder(){
     recording = false;
   }
 }
-#endif
+
+void Hardware::update_user_state(const nite::UserData& user, unsigned long long ts) {
+  if (user.isNew())
+    USER_MESSAGE("New")
+  else if (user.isVisible() && !g_visibleUsers[user.getId()])
+    USER_MESSAGE("Visible")
+  else if (!user.isVisible() && g_visibleUsers[user.getId()])
+    USER_MESSAGE("Out of Scene")
+  else if (user.isLost())
+    USER_MESSAGE("Lost")
+
+  g_visibleUsers[user.getId()] = user.isVisible();
+
+
+  if(g_skeletonStates[user.getId()] != user.getSkeleton().getState())
+  {
+    switch(g_skeletonStates[user.getId()] = user.getSkeleton().getState())
+    {
+    case nite::SKELETON_NONE:
+      USER_MESSAGE("Stopped tracking.")
+      break;
+    case nite::SKELETON_CALIBRATING:
+      USER_MESSAGE("Calibrating...")
+      break;
+    case nite::SKELETON_TRACKED:
+      USER_MESSAGE("Tracking!")
+      break;
+    case nite::SKELETON_CALIBRATION_ERROR_NOT_IN_POSE:
+    case nite::SKELETON_CALIBRATION_ERROR_HANDS:
+    case nite::SKELETON_CALIBRATION_ERROR_LEGS:
+    case nite::SKELETON_CALIBRATION_ERROR_HEAD:
+    case nite::SKELETON_CALIBRATION_ERROR_TORSO:
+      USER_MESSAGE("Calibration Failed... :-|")
+      break;
+    }
+  }
+}
+
+int Hardware::next() {
+
+  if(recording)
+    recorder();
+
+  return userTracker_.readFrame(&userTrackerFrame_);
+}
+
+void Hardware::get_users() {
+  const nite::Array<nite::UserData>& users = userTrackerFrame_.getUsers();
+
+  for (int i = 0; i < users.getSize(); ++i)
+  {
+    const nite::UserData& user = users[i];
+    update_user_state(user, userTrackerFrame_.getTimestamp());
+    if (user.isNew())
+    {
+      userTracker_.startSkeletonTracking(user.getId());
+    }
+    else if (user.getSkeleton().getState() == nite::SKELETON_TRACKED)
+    {
+      const nite::SkeletonJoint& head = user.getSkeleton().getJoint(nite::JOINT_HEAD);
+      if (head.getPositionConfidence() > .5)
+        printf("%d. (%5.2f, %5.2f, %5.2f) - Head found with condfidence %5.2f\n", user.getId(), head.getPosition().x, head.getPosition().y, head.getPosition().z, head.getPositionConfidence());
+    } else {
+      printf("%d. (%5.2f, %5.2f, %5.2f)\n", user.getId(), user.getCenterOfMass().x, user.getCenterOfMass().y, user.getCenterOfMass().z);
+    }
+  }
+} 
 
 cv::Mat Hardware::depth() {
-  if (simulate_) return simulated_depth_;
-#ifdef USE_NITE
   depthFrame_ = userTrackerFrame_.getDepthFrame();
 
   openni::DepthPixel *depthPixels = new openni::DepthPixel[depthFrame_.getHeight()*depthFrame_.getWidth()];
@@ -89,32 +124,8 @@ cv::Mat Hardware::depth() {
   cv::Mat depthImage(depthFrame_.getHeight(), depthFrame_.getWidth(), CV_16U, depthPixels);
 
   return depthImage;
-#endif
-  std::cerr << "### Hardware::depth()" << std::endl;
-  exit(-1);
 }
 
-// cv::Mat Hardware::rgb() {
-// if (simulate_) return simulated_rgb_;
-//   const libfreenect2::Frame* const rgb = frames_[libfreenect2::Frame::Color];
-//   return cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data).clone();
-// }
-
-// cv::Mat Hardware::ir() {
-// if (simulate_) return simulated_ir_;
-//   const libfreenect2::Frame* const ir = frames_[libfreenect2::Frame::Ir];
-//   return cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).clone();
-// }
-
-void Hardware::close() {
-  if (simulate_) return;
-#ifdef USE_NITE
-  depthFrame_.release();
-  nite::NiTE::shutdown();
-#endif
-}
-
-#ifdef USE_PCL
 pcl::PointCloud<pcl::PointXYZ>::Ptr Hardware::pcl(){  
 
   openni::DepthPixel *depthPixels = new openni::DepthPixel[depthFrame_.getHeight()*depthFrame_.getWidth()];
@@ -124,7 +135,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr Hardware::pcl(){
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-  float x,y;
+  float x,y,z;
 
   pointcloud->width = depthFrame_.getWidth(); //Dimensions must be initialized to use 2-D indexing 
   pointcloud->height = depthFrame_.getHeight();
@@ -135,17 +146,18 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr Hardware::pcl(){
       int depth_value = (int) depthImage.at<unsigned short>(j,i);
 
       // find the world coordinates
-      userTracker_.convertDepthCoordinatesToJoint(j, i, depth_value, &x, &y);
+       // userTracker_.convertDepthCoordinatesToJoint(j, i, depth_value, &x, &y);
+      convertDepthCoordinatesToWorld(j, i, depth_value, x, y, z);
 
       vertex.x   = (float) x;
       vertex.y   = (float) y;
-      vertex.z   = (float) depth_value;
+      vertex.z   = (float) z;
 
       // the point is pushed back in the cloud
       pointcloud->points.push_back( vertex );
-    }
+    }  
   }
-
+  
   return pointcloud;
 }
 
@@ -160,5 +172,51 @@ void Hardware::write_pcl(std::string path, pcl::PointCloud<pcl::PointXYZ>::Ptr p
   pcl::PLYWriter writer;
   writer.write(path + "/pcl_" + datetime_str() + ".ply", *pointcloud, false, false);
 }
-#endif
 
+void Hardware::close() {
+  depthFrame_.release();
+  nite::NiTE::shutdown();
+}
+
+void Hardware::convertDepthCoordinatesToWorld(int r, int c, float depth, float &x, float &y, float &z) const {
+
+  const float cx = 256.684;
+  const float cy = 207.085;
+  const float fx = 366.193;
+  const float fy = 366.193;
+
+  const float bad_point = std::numeric_limits<float>::quiet_NaN();
+  // const float cx(depth.cx), cy(depth.cy);
+  // const float fx(1/depth.fx), fy(1/depth.fy);
+  // float* undistorted_data = (float *)undistorted->data;
+
+  const float depth_val = depth/1000.0f; //scaling factor, so that value of 1 is one meter.
+  if (isnan(depth_val) || depth_val <= 0.001)
+  {
+    //depth value is not valid
+    x = y = z = bad_point;
+  }
+  else
+  {
+    x = (c + 0.5 - cx) * fx * depth_val/100000.0f;
+    y = (r + 0.5 - cy) * fy * depth_val/100000.0f;
+    z = depth_val;
+  }
+}
+
+void Hardware::convertJointCoordinatesToDepth(float x, float y, float z, float* pOutX, float* pOutY) const {
+
+  userTracker_.convertJointCoordinatesToDepth(x, y, z, pOutX, pOutY);
+}
+
+// void Hardware::convertJointCoordinatesToWorld(float x, float y, float z, float* pOutX, float* pOutY) const {
+
+//   userTracker_.convertDepthCoordinatesToJoint(x, y, z, pOutX, pOutY);
+//   convertDepthCoordinatesToWorld(j, i, depth_value, x, y, z);
+
+// }
+
+void Hardware::convertDepthCoordinatesToJoint(int x, int y, int z, float* pOutX, float* pOutY) const {
+
+  userTracker_.convertDepthCoordinatesToJoint(x, y, z, pOutX, pOutY);
+}
--- cc/hardware.h
+++ cc/hardware.h
@@ -3,63 +3,60 @@
 
 #include <memory>
 
-#ifdef USE_PCL
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
-#endif
 
 #include <opencv2/opencv.hpp>
 
-#ifdef USE_NITE
 #include "NiTE.h"
-#endif
+
+#define MAX_USERS 10
+
+#define USER_MESSAGE(msg) \
+    {printf("[%08llu] User #%d:\t%s\n",ts, user.getId(),msg);}
 
 class Hardware {
   public:
     // Also initializes Kinect and exits program in case of error.
-    Hardware(bool simulate = false);
+    Hardware();
 
-    // Waits for another frame. Returns 0 if successful, != 0 if timeout.
+    // Waits for another frame. Returns `false` in case of error.
     int next();
 
-    // // Returns IR data. Invalidated when `next()` is called.
-    // cv::Mat ir();
-    // // Returns rgb data. Invalidated when `next()` is called.
-    // cv::Mat rgb();
+    void get_users();
     // Returns depth data. Invalidated when `next()` is called.
     cv::Mat depth();
-
-    // Shuts down the device, irreversibly.
-    void close();
-
-#ifdef USE_PCL
     // Returns a point cloud.
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcl();
     // Writes a pcl to file
     void write_pcl(std::string path, pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud);
     // Starts a recording of each frame when  next() is called
     void record_pcl(const std::string path, const int nr_frames);
-#endif
+    // Convert XYZ world coordinate from depth map val of kinect v2
+    void convertDepthCoordinatesToWorld(int r, int c, float depth, float &x, float &y, float &z) const;
+    // Convert joint to depth map value
+    void convertJointCoordinatesToDepth(float x, float y, float z, float* pOutX, float* pOutY) const;
+    // Convert depth map value to joint coordinate
+    void convertDepthCoordinatesToJoint(int x, int y, int z, float* pOutX, float* pOutY) const;
+    // Shuts down the device, irreversibly.
+    void close();
 
-  private:
-    const bool simulate_;
-    cv::Mat simulated_depth_, simulated_ir_, simulated_rgb_;
+  private:   
     bool recording = false;
     int nr_rec_frames_;
+    openni::VideoFrameRef depthFrame_;
     std::string rec_path_;
     std::vector<std::string> rec_names_;
-
-#ifdef USE_NITE
-    openni::VideoFrameRef depthFrame_;
-    nite::UserTrackerFrameRef userTrackerFrame_;
-    nite::Status niteRc_;
-    nite::UserTracker userTracker_;
-#endif
-
-#ifdef USE_PCL
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> pointclouds_;
+
+    nite::UserTrackerFrameRef userTrackerFrame_;
+    nite::UserTracker userTracker_;
+
+    bool g_visibleUsers[MAX_USERS] = {false};
+    nite::SkeletonState g_skeletonStates[MAX_USERS] = {nite::SKELETON_NONE};
+    
     void recorder();
-#endif
+    void update_user_state(const nite::UserData& user, unsigned long long ts);
 
 };
 
