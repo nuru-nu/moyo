92ebf3218170b5ce7b66e368144d14258adda6bd
Andreas Steiner <andreassteiner@gmx.de>
2020-12-13 21:07:17 +0100

    Better heart signal.

--- py/nuru/hotplug/signals.py
+++ py/nuru/hotplug/signals.py
@@ -136,7 +136,7 @@ actions = dict(
 )
 
 animation = dict(
-    heart=S.TransientPulse('event', 'heart') | S.Lin(-5, 10) | S.Int() | S.Clip(),
+    heart=S.TransientPulse('event', 'heart') | S.RateLimit(5) | S.Tocos(),  #| S.Lin(-5, 10) | S.Int() | S.Clip(),
     heart_a=(
         S.Saw(hz=L.Named('arousal') | S.Lin(0, 2))
         | S.Lin(0, 3) | S.Clip() | S.Lin(0, 2) | S.Tocos()
