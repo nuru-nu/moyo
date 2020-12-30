ee38c7b0caf4394d12fb5d342c3bc188bda72910
Andreas Steiner <andreassteiner@gmx.de>
2020-12-30 21:56:45 +0100

    Adds minimal test.html

---
+++ static/test.js
@@ -0,0 +1,29 @@
+
+import { Kinect } from './kinect.js'
+
+const network = function () {
+  const data = {
+    signals: {
+      people: [],
+    },
+  }
+
+  const listeners = { signals: new Set() }
+
+  function tick() {
+    Array.from(listeners.signals).forEach(listener => listener(data.signals))
+    setTimeout(tick, 20)
+  }
+  tick()
+
+  return {
+    listenJson: function (which, listener) {
+      listeners[which].add(listener)
+    },
+    sender: function ({ people_override }) {
+      if (people_override) data.signals.people = people_override
+    },
+  }
+}()
+
+Kinect('#test', { network })
