1423f429274d02b572754d995d2a5ed5da8075e1
nuru <nuru@smokeandmirrors.ch>
2021-01-04 11:33:48 +0100

    Implements getip() on arch linux.

--- utils.sh
+++ utils.sh
@@ -4,3 +4,9 @@ if [ $(uname) = Darwin ]; then
         ping -c1 $1 | head -1 | sed -e's/.*(\(.*\)).*/\1/'
     }
 fi
+
+if [ $(uname -n) = sanduku ]; then
+    function getip() {
+        avahi-resolve-host-name -4 $1 | awk '{print $2}'
+    }
+fi
