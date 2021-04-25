8c2ac19762eec9fdd78115f53e7d0f4d04146620
nuru <nuru@smokeandmirrors.ch>
2021-04-25 11:33:33 +0200

    Adds launch_arduino.sh

---
+++ launch_arduino.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+. ./utils.sh
+
+CM=${CM:-C-m}
+SERVER=${SERVER:-127.0.0.1}
+
+tmux new-session -d -s nuru_arduino
+
+tmux splitw -v
+tmux send-keys './run nuru.integrator'
+tmux splitw
+tmux send-keys './run nuru.server --server_address 0.0.0.0'
+tmux selectp -L
+tmux send-keys './run smanmi.arduino --signal_port $(./run nuru.settings integrator_sig_port) --signal_name touch_raw --dev_glob "/dev/ttyACM0*"'
+
+tmux selectp -D
+tmux -CC a
