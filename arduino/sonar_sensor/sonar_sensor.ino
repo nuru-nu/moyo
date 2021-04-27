6af399a4a9bd5e20e2db39bf34931f7c462b910d
nuru <nuru@smokeandmirrors.ch>
2021-04-27 23:01:57 +0200

    New sonar arduino script without cmdMessenger

--- arduino/sonar_sensor/sonar_sensor.ino
+++ arduino/sonar_sensor/sonar_sensor.ino
@@ -1,74 +1,27 @@
-#include "CmdMessenger.h"
-
 #define TRIG_PIN 9
 #define ECHO_PIN 8
 
 float max_dist = 60.0;     // Centimeters
 long const pulse_time_max = (2 * (max_dist * 2)) / 0.0343;
 
-/* Define available CmdMessenger commands */
-enum {
-  get_sonar,
-  sonar_dist,
-  error,
-};
-
-/* Initialize CmdMessenger -- this should match PyCmdMessenger instance */
-const int BAUD_RATE = 9600;
-CmdMessenger c = CmdMessenger(Serial,',',';','/');
-
-/* Create callback functions to deal with incoming messages */
-
-/* callback */
-void on_get_sonar(void){
-    float single_dist = read_sonar();
-    c.sendBinCmd(sonar_dist, single_dist);
-}
-
-/* callback */
-void on_unknown_command(void){
-    c.sendCmd(error, "Command without callback.");
-}
-
-/* Attach callbacks for CmdMessenger commands */
-void attach_callbacks(void) { 
-  
-    c.attach(get_sonar, on_get_sonar);
-    c.attach(on_unknown_command);
-}
-
-
-void setup() {
+void setup()
+{
   // sanity check delay - allows reprogramming if accidently blowing power w/leds
   delay(2000);
+  
+  Serial.begin(57600);
 
   // setup ultra sonic sensor pins
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-
-  Serial.begin(BAUD_RATE);
-  attach_callbacks();  
-
 }
 
-void loop() {
-  c.feedinSerialData();
-  // long single_dist = read_sonar();
-
-  // if(single_dist != 0.0) {
-
-  //   if (single_dist <= max_dist){
-  //     // presence
-  //   }
-
-  //   Serial.print(max_dist);
-  //   Serial.print(", ");
-  //   Serial.println(single_dist);
-  // }
+void loop()
+{
+  Serial.println("S:" + String(read_sonar()));
 }
 
-float read_sonar() {
-
+int read_sonar() {
   // Create a 10ms pulse. Switch the trigger pin to HIGH for 10 microseconds
   // Ensure the trigger pin is off, by setting it to LOW for 2 microseconds
   digitalWrite(TRIG_PIN, LOW);
@@ -82,6 +35,5 @@ float read_sonar() {
 
   // Using the speed of sound you can calculate the distance from the duration of the flight of the pulse
   float distance_cm = (duration_us / 1e6 * 100 * 343) / 2.0;
-  return distance_cm;
+  return int(distance_cm);
 }
-
