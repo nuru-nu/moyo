4bca0e0cd461e1d3bf98c81272840fb8a80d79ae
nuru <nuru@smokeandmirrors.ch>
2021-05-09 19:23:07 +0200

    Restructuring nca.json slightly, adding "info".

--- py/nuru/presets.py
+++ py/nuru/presets.py
@@ -1,6 +1,7 @@
 """Manages configurable presets."""
 
 import json
+import glob
 import os
 
 from smanmi import util
@@ -8,10 +9,31 @@ from smanmi import util
 logger = util.createLogger('presets')
 
 NCA_PATH = os.path.join(os.path.dirname(__file__), 'nca.json')
+NCA_GLOB = os.path.join(
+    os.path.dirname(__file__),
+    os.path.pardir,
+    os.path.pardir,
+    'nca',
+    f'*.npy',
+)
+
+
+def _upgrade_presets(presets):
+    ret = {}
+    for k, v in presets.items():
+        if isinstance(v, str):
+            v = dict(nca=v)
+        # if '_emotion' not in v and '_' in k:
+        #     v['_emotion'] = k.split('_')[-1]
+        ret[k] = v
+    return ret
 
 
 def get_nca():
-    return json.load(open(NCA_PATH))
+    ret = json.load(open(NCA_PATH))
+    ret['presets'] = _upgrade_presets(ret['presets'])
+    ret['all_names'] = [path.split('/')[-1][:-4] for path in glob.glob(NCA_GLOB)]
+    return ret
 
 
 def set_nca(name, d):
