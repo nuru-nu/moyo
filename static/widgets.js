9bc687125837c477b898990042f1089b032fad07
Andreas Steiner <andreassteiner@gmx.de>
2021-05-13 20:24:40 +0200

    Adds heart simulation.

--- static/widgets.js
+++ static/widgets.js
@@ -423,6 +423,8 @@ export const Animations = (output, {defs, network}) => {
       ui.choice('anim_sig', { network, values: ['one', 'closest', 'rnd1', 'arousal'] }),
       '...',
       ui.toggle('anim_heart', { network, text: 'heart' }),
+      ui.toggle('heart_sim', { network, text: 'sim' }),
+      '...',
       ui.toggle('anim_into', { network, text: 'into' }),
     ),
   )).into(output).els
