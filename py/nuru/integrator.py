5d33af06832c942a39c89661e462b550cdc1eae4
Andreas Steiner <andreassteiner@gmx.de>
2021-05-13 17:12:32 +0200

    Adds index2.html KOSMOS WIP draft.

--- py/nuru/integrator.py
+++ py/nuru/integrator.py
@@ -46,6 +46,7 @@ class Integrator:
             ),
             sig_out_ports=(
                 (args.server_address, settings.server_sig_port),
+                settings.server2_sig_port,
                 settings.player_sig_port,
                 settings.player2_sig_port,
                 settings.dmx_sig_port,
