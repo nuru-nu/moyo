283c988ed35b895a607df3cf806b3c5aad5206ad
nuru <nuru@smokeandmirrors.ch>
2021-05-15 20:59:59 +0200

    Adds anim_dark button.

--- static/animations.js
+++ static/animations.js
@@ -44,6 +44,8 @@ export const Animations = (output, {network, defs}) => {
       ui.range('v2', { network, text: null }),
     ),
     ui.h(
+      ui.toggle('anim_dark', { network, text: 'dark' }),
+      '...',
       ui.choice('anim_sig', { network, values: ['one', 'closest', 'rnd1', 'arousal'] }),
       '...',
       ui.toggle('anim_heart', { network, text: 'heart' }),
