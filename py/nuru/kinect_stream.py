40072f20a037cc78f4a8d405985a1e363a70cd07
Gabriel le Roux <lrxgab005@gmail.com>
2023-04-28 20:25:40 +0200

    Made kinect lib

--- py/nuru/kinect_stream.py
+++ py/nuru/kinect_stream.py
@@ -3,17 +3,13 @@ import numpy as np
 import cv2
 import os
 import time
-from pylibfreenect2 import Freenect2, SyncMultiFrameListener
-from pylibfreenect2 import FrameType, Registration, Frame
 from ultralytics import YOLO
 from collections import deque
 from datetime import datetime
 import colorsys
-import open3d as o3d
-import json
 from collections import defaultdict
 
-from nuru import settings, people_tracking
+from nuru import settings, people_tracking, kinect_lib
 from smanmi import network, util
 
 logger = util.createLogger('kinect', debug=False)
@@ -35,7 +31,7 @@ parser.add_argument(
     '--yolo_stream', type=str, default='ir_rgb', help="Kinect stream name for YOLO."
 )
 parser.add_argument(
-    '--yolo_class_ids', type=str, nargs='*', default=None, help='YOLO class ids to detect. 0 for people.'
+    '--yolo_class_ids', type=int, nargs='*', default=None, help='YOLO class ids to detect. 0 for people.'
 )
 parser.add_argument(
     '--data_out', type=str, default=settings.kinect_data_path, help="Data output folder."
@@ -57,206 +53,6 @@ parser.add_argument(
 args = parser.parse_args()
 
 PERSON_ID = 0
-
-class Kinect:
-    def __init__(
-            self, 
-            streams=["color", "depth", "ir"], 
-            shimono_trafo_path=None, 
-            output_dir=None, 
-            flip=False, 
-            width = 512, 
-            height = 424
-        ):
-        """Initialize the Kinect device."""
-
-        self.freenect = Freenect2()
-        num_devices = self.freenect.enumerateDevices()
-        assert num_devices > 0, "No Kinect device detected"
-
-        serial = self.freenect.getDeviceSerialNumber(0)
-        self.device = self.freenect.openDevice(serial)
-        
-        self.streams = streams
-        self.output_dir = output_dir
-        self.flip = flip
-        self.width = width
-        self.height = height
-
-        frame_types = 0
-        if "ir_rgb" in streams:
-            streams += ["color", "ir"]
-        if "color" in streams:
-            frame_types |= FrameType.Color
-        if "depth" in streams:
-            frame_types |= FrameType.Depth
-        if "ir" in streams:
-            frame_types |= FrameType.Ir
-
-        self.listener = SyncMultiFrameListener(frame_types)
-        self.device.setColorFrameListener(self.listener)
-        self.device.setIrAndDepthFrameListener(self.listener)
-
-        self.device.start()
-
-        self.registration = Registration(
-            self.device.getIrCameraParams(), self.device.getColorCameraParams()
-        )
-
-        self.undistorted = Frame(width, height, 4)
-        self.registered = Frame(width, height, 4)
-
-        self.shimoni_trafo = self.load_transform(shimono_trafo_path)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        """Get the next frames from the Kinect device."""
-
-        frames = self.listener.waitForNewFrame()
-
-        # Apply registration if both color and depth are enabled
-        if "color" in self.streams and "depth" in self.streams:
-            self.registration.apply(
-                frames["color"], frames["depth"], self.undistorted, self.registered
-            )
-
-        # Create output images
-        output = {}
-        if "color" in self.streams:
-            color = frames["color"]
-            output["color"] = cv2.cvtColor(
-                color.asarray(dtype=np.uint8), cv2.COLOR_RGBA2BGR
-            )
-            output["color"][:,:,[0, 2]] = output["color"][:,:, [2, 0]] # BGR to RGB
-
-            output["scaled-color"] = cv2.cvtColor(
-                self.registered.asarray(dtype=np.uint8), cv2.COLOR_RGBA2BGR
-            )
-            output["scaled-color"][:,:,[0, 2]] = output["scaled-color"][:,:, [2, 0]] # BGR to RGB
-
-        if "depth" in self.streams:
-            depth = frames["depth"]
-            output["depth"] = cv2.normalize(
-                depth.asarray(dtype=np.float32),
-                None,
-                0,
-                1,
-                cv2.NORM_MINMAX,
-                cv2.CV_32F,
-            )
-
-        if "ir" in self.streams:
-            ir = frames["ir"]
-            output["ir"] = cv2.normalize(
-                ir.asarray(dtype=np.float32),
-                None,
-                0,
-                1,
-                cv2.NORM_MINMAX,
-                cv2.CV_32F,
-            )
-        if "ir_rgb" in self.streams:
-            output["ir_rgb"] = self.ir_enhance(output["scaled-color"], output["ir"])
-
-        if self.flip:
-            for k, v in output.items():
-                output[k] = cv2.flip(v, 1)
-                output[k] = cv2.flip(v, 0)
-
-        self.listener.release(frames)
-        return output
-    
-    def ir_enhance(self, rgb, ir):
-        """Enhance the rgb image by adaptively adding IR to the V channel of the HSV image."""
-
-        hsv_img = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV).astype(np.float32) / 255.0
-        hsv_img[:,:,2] += np.clip(ir - hsv_img[:,:,2], 0, 1)
-        rgb_img = cv2.cvtColor((hsv_img * 255).astype(np.uint8), cv2.COLOR_HSV2BGR)
-
-        return rgb_img
-    
-    def get_mean_coords_for_segments(self, seg_labels):
-        """Get the mean 3D location of each segmentation label."""
-
-        for detections in seg_labels.values():
-            for detection in detections:
-                mask = np.zeros((self.width, self.height), dtype=np.uint8)
-                cv2.fillPoly(mask, [detection["2D_outline"]], 255)
-
-                points_3d = []
-                for seg_point in np.argwhere(mask == 255):
-                    c, r = seg_point
-                    x, y, z = self.registration.getPointXYZ(kinect.undistorted, c, r)
-                    if not np.isnan(x) and not np.isnan(y) and not np.isnan(z):
-                        points_3d.append([x, y, z])
-                
-                if len(points_3d) == 0:
-                    continue
-                
-                detection["3D_point"] = np.mean(points_3d, axis=0)
-                detection["3D_shimoni"] = self.get_point_shimino_space(*detection["3D_point"])
-                detection["cm"] = detection["3D_shimoni"] # HACK: for compatibility with old code
-
-        return seg_labels
-    
-    def get_point_3d(self, c, r):
-        """Get the 3D location of a point in the depthmap."""
-
-        x, y, z = self.registration.getPointXYZ(kinect.undistorted, c, r)
-
-        if np.isnan(x) or np.isnan(y) or np.isnan(z):
-            return None
-        
-        return x, y, z
-    
-    def get_point_shimino_space(self, x, y, z):
-        """Gets 3D point in kinect space and transforms to shimoni space."""
-        
-        assert self.shimoni_trafo is not None, "Shimoni transformation not loaded"
-
-        x_s, y_s, z_s, w = self.shimoni_trafo @ np.array([x, y, z, 1.0])
-
-        return x_s/w, y_s/w, z_s/w
-    
-    def save_point_cloud(self):
-        """Save the current point cloud to a PLY file."""
-
-        assert self.output_dir is not None, "Output directory not specified"
-
-        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-        ply_path = os.path.join(self.output_dir, f'pcl_{timestamp}.ply')
-
-        # Get the point cloud
-        pts = []
-        for r in range(self.width ):
-            for c in range(self.height):
-                point = self.get_point_3d(c, r)
-                if point is not None:
-                    pts.append(point)
-
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(pts)
-
-        o3d.io.write_point_cloud(ply_path, pcd, write_ascii=True)
-
-    def load_transform(self, path):
-        """Load the transformation matrix from the given path."""
-
-        if path is None:
-            return None
-        
-        with open(path, 'r') as file:
-            data = json.load(file)
-
-        return np.array(data['world_matrix'])
-
-    def close(self):
-        """Close the Kinect device."""
-
-        self.device.stop()
-        self.device.close()
 
 class ImageAnnotator:
     def __init__(self, font_size=1, font_thickness=2, line_thickness=2, num_colors=8):
@@ -434,7 +230,7 @@ if __name__ == "__main__":
     # Initialize modules
     video_writer = VideoWriter(args.data_out, args.rec_streams)
     dynamic_fps = FPSCounter()
-    kinect = Kinect(
+    kinect = kinect_lib.Kinect(
         streams=list(streams), 
         shimono_trafo_path=args.shimono_trafo_path, 
         output_dir=args.data_out, 
@@ -445,7 +241,7 @@ if __name__ == "__main__":
     if args.run_yolo:
         ys = YOLOSegmentation(settings.yolo_models[args.yolo_model], args.yolo_class_ids)
         annotator = ImageAnnotator()
-        tracker = people_tracking.Tracker(args.max_person_away_frames)
+        # tracker = people_tracking.Tracker(args.max_person_away_frames)
 
     # Start Kinect frame stream   
     for frame_data in kinect:
@@ -464,15 +260,16 @@ if __name__ == "__main__":
             img_segments = kinect.get_mean_coords_for_segments(img_segments)
 
             # Track people only if cm coordinates are available
-            tracked_people = tracker.update(
-                [seg for seg in img_segments[PERSON_ID] if "cm" in seg]
-            )
+            tracked_people = img_segments
+            # tracked_people = tracker.update(
+            #     [seg for seg in img_segments[PERSON_ID] if "cm" in seg]
+            # )
 
             # Draw detections
             annotator.draw_detections(frame_data[args.yolo_stream], tracked_people)
 
             # Send tracked people to integrator
-            network.send(settings.integrator_sig_port, dict(people_sensor=tracked_people))
+            # network.send(settings.integrator_sig_port, dict(people_sensor=tracked_people))
 
         # Start/stop record video when 's' is pressed
         if key == ord('s'):
