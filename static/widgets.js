3626f3d7c2adbb362ec9e511d4fb9d3b303f1224
Gabriel le Roux <lrxgab005@gmail.com>
2023-12-25 20:25:50 +0200

    Added happy growl button to web ui

--- static/widgets.js
+++ static/widgets.js
@@ -105,7 +105,7 @@ export const Subsample = (output, {network}) => {
 export const Cmd = (output, {network, defs}) => {
   const actions = [
     'fc=0', 'fc=1', 'fc=2', 'fc=3', 'next',
-    'growl=angry', 'sub=on', 'sub=off', 'growl=hole',
+    'growl=angry', 'growl=happy', 'sub=on', 'sub=off',
     'dream',
   ]
   const els = Header('cmd', h.div().of(
