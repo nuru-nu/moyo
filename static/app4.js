b6a955c5311e559e06d8f7c2b6da84c8d7b3769a
Gabriel le Roux <lrxgab005@gmail.com>
2024-01-17 22:03:50 +0100

    Created new web ui for Kraftwerk camera man

---
+++ static/app4.js
@@ -0,0 +1,31 @@
+
+import { Network } from './smanmi/network.js'
+import { Css, ImageGPT, AffectWordButtons, AnimControl} from './widgets.js'
+import { Dump } from './smanmi/monitor.js'
+import { Leds } from './leds.js'
+
+fetch('/defs').then(resp => resp.json()).then(defs => {  
+  let leds = Leds('#leds', defs)
+  const network = Network(null, {secondary: true})
+  const sz = 0.33 * Math.min(window.innerWidth, window.innerHeight)
+  ImageGPT('#image', {
+    network,
+    headless: true,
+  })
+  Css('#css', {
+    network,
+    readonly: true,
+    headless: true,
+    hidestate: true,
+    width: sz, height: sz,
+  })
+  AffectWordButtons('#affectwords', {network, defs})
+  Dump("#signals", {network})
+  AnimControl('#anim_control', {network, defs})
+  network
+  .listen('animation', leds.listener)
+})
+
+document.addEventListener('gesturestart', function (e) {
+  e.preventDefault();
+});
