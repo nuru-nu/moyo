97effa47b25706e2841b4cf6c26ffc174d81e47f
Gabriel le Roux <lrxgab005@gmail.com>
2024-01-17 22:04:41 +0100

    Adjusted tmux terminal layout for Kraftwerk

--- launch_kraftwerk_sim.sh
+++ launch_kraftwerk_sim.sh
@@ -16,22 +16,29 @@ tmux selectp -L
 # row 1
 tmux send-keys './run nuru.video_stream_gpt --display_stream --img_gpt_stream' $CM
 # row 2
-tmux splitw -p 60
+tmux splitw -p 77
 tmux send-keys '(cd fadecandy; ./fcserver-osx config.json)' $CM
+# row 3
+tmux splitw -p 66
+tmux send-keys './run nuru.midi' $CM
 # row 4
 tmux splitw
-tmux send-keys './run nuru.midi' $CM
+tmux send-keys "./run nuru.integrator" $CM
+
 
 ## column 2
 tmux selectp -R
-# row 1
-tmux send-keys "./run nuru.integrator" $CM
-# row 2
-tmux splitw -p 60
+# row 1 - Host outward facing server
+tmux send-keys "ssh smanmi@nuru.nu -R9898:localhost:8081" $CM
+# row 2 - Full back end server
+tmux splitw -p 77
 tmux send-keys "./run nuru.server --server_address=${SERVER} --fadecandy" $CM
-# row 3
+# row 3 - Outward facing web server
+tmux splitw -p 66
+tmux send-keys "./run nuru.server --fps=0 --instance=2 --index=index3.html --port=8081" $CM
+# row 4 - Camera control web server
 tmux splitw
-tmux send-keys "./run nuru.server --fps=0 --instance=3 --index=index3.html --port=8081" $CM
+tmux send-keys "./run nuru.server --fps=0 --instance=3 --index=index4.html --port=8082" $CM
 
 tmux selectp -D
 tmux attach-session
